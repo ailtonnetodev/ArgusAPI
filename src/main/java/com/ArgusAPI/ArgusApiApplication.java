d2bc3dc0c99eedd4aa3100e00b93603223f917cc
ailtonnetodev <ailtonnetto8319@gmail.com>
2024-11-05 01:05:32 -0300

    Checkout from main, add Configs

---
+++ src/main/java/com/ArgusAPI/ArgusApiApplication.java
@@ -0,0 +1,13 @@
+package com.ArgusAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArgusAPI/ArgusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArgusAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
